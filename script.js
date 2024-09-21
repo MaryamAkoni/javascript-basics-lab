6681ebf9d704bddad0fb3791746b8113b7bfbf63
MaryamAkoni <akoniodunayo@gmail.com>
2024-09-20 21:19:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let greeting = "Hello Professor"
+console.log(greeting);
+
+var studentName = "Maryam";
+var studentDob = 2002;
+let isEnrolled = true;
+
+console.log(studentName);
+console.log(studentDob);
+console.log(isEnrolled);
+
+let name = "Odunayo";
+let age = 22;
+let message = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(message);
+
+let distanceHome = 25.8;
+let unsafe = false;
+
+console.log(distanceHome);
+console.log(unsafe);
+
+let person = {
+    firstName: "Maryam",
+    lastName: "Akoni",
+    age: 22
+};
+console.log(person.lastName);
+
+let fruits = ['Apple', 'Strawberry', 'Watermelon'];
+console.log(fruits[0]); // Accesses the first element, output: 'Red'
+fruits.push('Mango'); // Adds a new element at the end
+console.log(fruits); // Output: ['Red', 'Green', 'Blue', 'Yellow']
+fruits.pop();
+console.log(fruits);
+
+console.log(3 * 5);
+console.log(15 / 3);
+console.log(10 - 5);
+console.log(2 + 2);
+console.log(19 % 3);
+
+let x = 2;
+let y = 3;
+console.log("2" == x);
+console.log("3" === x);
+console.log(x != y);
+console.log(x < y);
+console.log(x > y); 
+console.log(y <= x);
+console.log(x >= y);
+
+let a = false;
+let b = true;
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+
